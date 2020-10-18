77e14b22dafc54495ea00567bca72a0b98183a99
Kirill Dudnik <prog.me.2017@gmail.com>
2020-10-18 19:50:46 +0600

    The project was created! The game is about a character who needs to collect pieces of uranium and carry them to the reactor to get energy. - created a simple player - cube with movement and turns; - created a terrain with grass level; - created a basic Shader with and without a texture; - created a prefab of the stone with the model; - the project is currently being compiled on a PC! -------------- Проект создан! Игра про персонажа, которому необходимо собирать кусочки урана и носить их к реактору, чтобы получить энергию. - создал простого игрока - куб с передвижением и поворотами; - создал местность с уровнем травы; - создал базовый шейдер с текстурой и без неё; - создал префаб камня с моделью; - проект сейчас компилируется на ПК!

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    private Vector3 _distanceToPlayer;
+    void Start()
+    {
+        _distanceToPlayer = transform.position - _player.transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+    }
+
+    internal void Move()
+    {
+        transform.position = _player.transform.position + _distanceToPlayer;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainCamera;
+    private Camera camera;
+    [SerializeField] private float _moveSpeed = 4f;
+    Action OpenMouthDel;
+
+    void Start()
+    {
+        camera = _mainCamera.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            Movement();
+        }
+        RotateOnMove();
+    }
+
+    private Vector3 GetPlayerOffset()
+    {
+        float move = Input.GetAxis("Vertical");
+        Vector3 offset = new Vector3(move, 0.0f, 0.0f);
+        return offset;
+    }
+    private Vector3 Movement()
+    {
+        var translateTo = (GetPlayerOffset() * _moveSpeed * Time.fixedDeltaTime);
+        transform.Translate(translateTo);
+        return translateTo;
+    }
+    private void RotateOnMove()
+    {
+        float moveRotate = Input.GetAxis("Horizontal");
+        transform.Rotate(0.0f, moveRotate, 0.0f);
+    }
+}
