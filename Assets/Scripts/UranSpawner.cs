c51993aa587602e226c6ef52287bdb6cdf5794f8
Kirill Dudnik <prog.me.2017@gmail.com>
2020-10-20 16:26:01 +0600

    Update 1: Nuclear reactor! / Обновление 1: Ядерный реактор! - imported the object and texture of the reactor; - added shaders for reactor, water reactor, glass reactor; - added and configured the materials of the reactor; - added a green glow; - added a Collider for the reactor; - created a prefab of the reactor; other: - created a Spawner for Uranus; - placed the Uranus Spawner at the origin of the map coordinates; - automatic calculation of spawn coordinates based on map size; ----------------------------- - импортировал объект и текстуры реактора; - добавил шейдеры для реактора, воды реактор, стекла реактора; - добавил и настроил материалы реактора; - добавил свечение зеленого цвета; - добавил коллайдер для реактора; - создал префаб реактора; другое: - создал спавнер для урана; - разместил спавнер урана в началах координат карты; - автоматический расчёт координат спавна от размера карты;

---
+++ Assets/Scripts/UranSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UranSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _terrain;
+    [SerializeField] private GameObject _uranRock;
+    [SerializeField] private GameObject _uranRockContainer;
+    private Vector3 _terrainSize;
+    private double _terrainWidth;
+    private double _terrainLength;
+    private double _terrainVerticalOffset;
+
+    void Start()
+    {
+        _terrainSize = _terrain.GetComponent<Terrain>().terrainData.size;
+        _terrainWidth = _terrainSize.x;
+        _terrainLength = _terrainSize.z;
+        _terrainVerticalOffset = _terrain.transform.position.y;
+
+        for (int i = 0; i < 100; i++)
+        {
+            SpawnUran(_terrainWidth, _terrainLength);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SpawnUran(double terrWidth, double terrLength)
+    {
+        var randomXPos = Random.Range(0, (float)terrWidth);
+        var randomZPos = Random.Range(0, (float)terrLength);
+        var verticalPos = (float)_terrainVerticalOffset + 20;
+
+        var spawnPosition = new Vector3(randomXPos, verticalPos, randomZPos);
+
+        Instantiate(_uranRock, spawnPosition, Quaternion.identity, _uranRockContainer.transform);
+    }
+}
